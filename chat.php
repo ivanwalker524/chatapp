6477574c8f8b8c8473ddb609def2b825d01fd5e8
ivanwalker524 <ivanwalker524@gmail.com>
2022-07-28 21:46:52 +0300

    some esues

---
+++ chat.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Realtime Chat App | CodingNepal</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/c98faec581.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <div class="wrapper">
+        <section class="chat-area">
+            <header>
+                <a href="#" class="back-icon"><i class="fas fa-arrow-left"></i></a>
+                <img src="img.jpg" alt="">
+                <div class="details">
+                    <span>Ivan Walker</span>
+                    <p>Active now</p>
+                </div>
+            </header>
+            <div class="chat-box">
+                <div class="chat outgoing">
+                    <div class="details">
+                        <p>Lorem isum dolar sit amet, consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+                <div class="chat incoming">
+                    <img src="img.jpg" alt="">
+                    <div class="details">
+                        <p>Lorem isum dolar sit amet, consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+                <div class="chat outgoing">
+                    <div class="details">
+                        <p>Lorem isum dolar sit amet, consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+                <div class="chat incoming">
+                    <img src="img.jpg" alt="">
+                    <div class="details">
+                        <p>Lorem isum dolar sit amet, consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+                <div class="chat outgoing">
+                    <div class="details">
+                        <p>Lorem isum dolar sit amet, consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+                <div class="chat incoming">
+                    <img src="img.jpg" alt="">
+                    <div class="details">
+                        <p>Lorem isum dolar sit amet, consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+                <div class="chat outgoing">
+                    <div class="details">
+                        <p>Lorem isum dolar sit amet, consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+                <div class="chat incoming">
+                    <img src="img.jpg" alt="">
+                    <div class="details">
+                        <p>Lorem isum dolar sit amet, consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+                <div class="chat outgoing">
+                    <div class="details">
+                        <p>Lorem isum dolar sit amet, consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+                <div class="chat incoming">
+                    <img src="img.jpg" alt="">
+                    <div class="details">
+                        <p>Lorem isum dolar sit amet, consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+            </div>
+            <form action="#" class="typing-area">
+                <input type="text" placeholder="Type a a message here...">
+                <button><i class="fab fa-telegram-plane"></i></button>
+            </form>
+        </section>
+    </div>
+</body>
+
+</html>
---
+++ signin.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Realtime Chat App | CodingNepal</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/c98faec581.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="wrapper">
+        <section class="form login">
+            <header>Realtime Chat App</header>
+            <form action="#" autocomplete="off">
+                <div class="error-txt"></div>
+                    <div class="field input">
+                        <label for="">Email Address</label>
+                        <input type="text" name="email" placeholder="Enter your email">
+                    </div>
+                    <div class="field input">
+                        <label for="">Password</label>
+                        <input type="password" name="password" placeholder="Enter new password">
+                        <i class="fas fa-eye"></i>
+                    </div>
+                    <div class="field button ">
+                        <input type="submit" value="Continue to Chat">
+                    </div>
+                    <div class="link">Not yet signed up? <a href="index.php">signup now</a></div>
+                </form>
+            </section> 
+    </div>
+
+    <script src="javascript/pass-show-hide.js"></script>
+    <script src="javascript/login.js"></script>
+</body>
+</html>
